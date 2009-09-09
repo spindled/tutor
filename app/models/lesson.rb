7bc40b3f544ed2822b168561a205a0317379424e
David Spindle <spindled@gmail.com>
2009-09-09 08:45:54 -0400

    Posts now get deleted when the lesson is deleted

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,5 +1,5 @@
 class Lesson < ActiveRecord::Base
-  has_many :posts
+  has_many :posts, :dependent => :destroy 
   
   validates_uniqueness_of :title
   validates_presence_of :title, :description, :content
