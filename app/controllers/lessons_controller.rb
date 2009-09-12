b7c3b046daeed67d55ef89b34f70c9ae791e24fa
David Spindle <spindled@gmail.com>
2009-09-12 02:03:47 -0400

    Cleaned up controller docs

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,78 +1,44 @@
 class LessonsController < ApplicationController
-  # GET /lessons
-  # GET /lessons.xml
+
   def index
     @lessons = Lesson.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @lessons }
-    end
   end
 
   def show
     @lesson = Lesson.find(params[:id])
   end
 
-  # GET /lessons/new
-  # GET /lessons/new.xml
   def new
     @lesson = Lesson.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @lesson }
-    end
   end
 
-  # GET /lessons/1/edit
   def edit
     @lesson = Lesson.find(params[:id])
   end
 
-  # POST /lessons
-  # POST /lessons.xml
   def create
     @lesson = Lesson.new(params[:lesson])
-
-    respond_to do |format|
-      if @lesson.save
-        flash[:notice] = 'Lesson was successfully created.'
-        format.html { redirect_to(@lesson) }
-        format.xml  { render :xml => @lesson, :status => :created, :location => @lesson }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @lesson.errors, :status => :unprocessable_entity }
-      end
+    if @lesson.save
+      flash[:notice] = 'Lesson was successfully created.'
+      redirect_to(@lesson)
+    else
+      render :action => :new
     end
   end
 
-  # PUT /lessons/1
-  # PUT /lessons/1.xml
   def update
     @lesson = Lesson.find(params[:id])
-
-    respond_to do |format|
-      if @lesson.update_attributes(params[:lesson])
-        flash[:notice] = 'Lesson was successfully updated.'
-        format.html { redirect_to(@lesson) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @lesson.errors, :status => :unprocessable_entity }
-      end
+    if @lesson.update_attributes(params[:lesson])
+      flash[:notice] = 'Lesson was successfully updated.'
+      redirect_to(@lesson)
+    else
+      render :action => :edit
     end
   end
 
-  # DELETE /lessons/1
-  # DELETE /lessons/1.xml
   def destroy
     @lesson = Lesson.find(params[:id])
     @lesson.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(lessons_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(lessons_url)
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,42 +1,8 @@
 class PostsController < ApplicationController
   before_filter :find_lesson
   
-  # GET /posts
-  # GET /posts.xml
   def index
     @posts = @lesson.posts
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @posts }
-    end
-  end
-
-  # GET /posts/1
-  # GET /posts/1.xml
-  def show
-    @post = Post.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @post }
-    end
-  end
-
-  # GET /posts/new
-  # GET /posts/new.xml
-  def new
-    @post = Post.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @post }
-    end
-  end
-
-  # GET /posts/1/edit
-  def edit
-    @post = Post.find(params[:id])
   end
 
   def create
@@ -48,40 +14,17 @@ class PostsController < ApplicationController
     end
   end
 
-  # PUT /posts/1
-  # PUT /posts/1.xml
-  def update
-    @post = Post.find(params[:id])
-
-    respond_to do |format|
-      if @post.update_attributes(params[:post])
-        flash[:notice] = 'Post was successfully updated.'
-        format.html { redirect_to(@post) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @post.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /posts/1
-  # DELETE /posts/1.xml
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(posts_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to @lesson
   end
   
   private 
   
   def find_lesson
-    lesson_id = params[:lesson_id]
-    return(redirect_to(lessons_url)) unless lesson_id
-    @lesson = Lesson.find(lesson_id)
+    @lesson_id = params[:lesson_id]
+    return(redirect_to(lessons_url)) unless @lesson_id
+    @lesson = Lesson.find(@lesson_id)
   end
 end
